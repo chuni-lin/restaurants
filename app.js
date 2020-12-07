3d8d35764efc803c75ef1c7110162ca557f32c34
chuni-lin <william811566@gmail.com>
2020-12-07 15:20:04 +0800

    update setting

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
