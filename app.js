970dc6d0112aaaf9360b8d0a4279ea417d394990
chuni-lin <william811566@gmail.com>
2020-12-15 15:58:13 +0800

    feat: modify index and show.handlebars

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 const mongoose = require('mongoose')
 const db = mongoose.connection
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const Restaurant = require('./models/restaurant')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use((bodyParser.urlencoded({ extended: true })))
 app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
@@ -19,13 +21,22 @@ mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,
 db.on('error', () => console.log('MongoDB error!'))
 db.once('open', () => console.log('MongoDB connected!'))
 
+//載入資料庫
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
+
+//render show.handlebars
 app.get('/restaurants/:id', (req, res) => {
-  const restaurant = restaurantList.results.find(item => item.id.toString() === req.params.id)
-  res.render('show', { restaurant })
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
 })
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(item =>
