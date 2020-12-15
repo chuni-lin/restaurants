264b59472252c002dc6bb7bce1019084f46b80bd
chuni-lin <william811566@gmail.com>
2020-12-15 17:48:23 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -29,13 +29,25 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+// 新增餐廳
+app.get('/restaurants/create', (req, res) => res.render('create'))
+app.post('/restaurants', (req, res) => {
+  if (req.body.image.length === 0) { req.body.image = 'https://www.teknozeka.com/wp-content/uploads/2020/03/wp-header-logo-33.png' }
+  const restaurant = req.body
+  return Restaurant.create(restaurant)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 //render show.handlebars
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.error(error))
 })
+
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
